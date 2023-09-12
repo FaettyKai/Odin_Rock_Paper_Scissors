a1f17896d403fc6aa02e5454e9f086793878ef95
Michael <140651581+FaettyKai@users.noreply.github.com>
2023-09-12 00:22:31 -0500

    Got down main function of game. Now returns statements upon winning or losing.

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-function number(x) {
-	x = Math.floor(Math.random() * 3) + 1
+function random(x) {
+	x = Math.floor(Math.random() * 3) + 1 
   return x
 }
 
-function cpuSelection(y) { 
-	if(number() === 1)
+function cpuSelection(y) {
+	if(random() === 1)
   	y = "rock"
-  else if(number() === 2)
+  else if(random() === 2)
   	y = "paper"
-  else 
-  	y = "scissors"
-  return y
-}
-
-function player() {
-	z = prompt("what is your selection?")
-	if(z === "rock" || z === "paper" || z === "scissors")
-  	return z
   else
-  	return alert("do you not know how to play?")
+  	y = "scissors"
+  return y 
 }
 
+function playerSelect() {
+	prompt = prompt("what you pickin")
+  if(prompt === "rock" || prompt === "paper" || prompt === "scissors")
+  	return prompt
+  else 
+  	return alert("What. Don't tell me you dont know how to play Rock, Paper, Scissors")
+}
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = player()
+  playerSelection = playerSelect()
   computerSelection = cpuSelection()
-  if(playerSelection === computerSelection) 
-  	return alert("draw")
-  else if(playerSelection != computerSelection)
-  	return alert("no draw")
+  if(playerSelection === computerSelection)
+  	return alert("draw.")
+  else if((playerSelection === "rock" && computerSelection === "paper") || 
+  				 (playerSelection === "paper" && computerSelection === "scissors") ||
+           (playerSelection === "scissors" && computerSelection === "rock"))
+  	return alert("hah. You suck loser") 	
+  else if((playerSelection === "rock" && computerSelection === "scissors") || 
+  				 (playerSelection === "paper" && computerSelection === "rock") ||
+           (playerSelection === "scissors" && computerSelection === "paper"))
+    return alert("pft. You got lucky")       
 }
+playRound()
+
 
